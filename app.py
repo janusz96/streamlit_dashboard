414d911292cca38e72e0a859ef59ce1ea6382796
Janusz Zawistowski <janusz@MacBook-Pro-Janusz.local>
2025-02-25 19:48:51 +0100

    Pierwsza wersja aplikacji

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard", layout="wide")
+
+st.title("📊 Mój pierwszy dashboard")
+st.sidebar.header("Opcje")
+st.write("To jest przykładowa aplikacja Streamlit!")
