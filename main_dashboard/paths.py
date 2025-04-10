a2f83c9fa281ec221be52d6b9538eb88af8249a8
janusz96 <janaszawias@gmail.com>
2025-04-10 13:48:46 +0200

    Ignore main_dashboard/paths.py

--- main_dashboard/paths.py
+++
@@ -1,4 +0,0 @@
-path_raw_data = "/Users/janusz/Desktop/Olta/2024 - analiza tapicerni/czas_tapicernia.xls"
-path_cennik = "1bO-nzFUBkoyOUJQtPaeVw6rhyWyONU3_"
-path_komisja_skory = "1IpKxjCIpt9AFA581LaxM2cYefiS_twMm"
-path_poprawne_bryly = "1-du9EjEt8SM_6Ntg-FHrULA3y9IL2uld"
