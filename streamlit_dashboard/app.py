6d1b48bc50ad07edffdf3b8ca1ae4531cf75b3d4
janusz96 <janaszawias@gmail.com>
2025-04-10 12:50:04 +0200

    dodanie req

--- streamlit_dashboard/app.py
+++ streamlit_dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime as dt
-from data_functions import *
+from streamlit_dashboard.data_functions import *
 from paths import *
 
 st.set_page_config(page_title="Dashboard", layout="wide")
